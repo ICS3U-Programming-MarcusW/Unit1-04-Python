27769b65cd9470ed4949282779b96ebc3c6abd4e
Marcus Wehbi <marcus.wehbi@stu.ocsb.ca>
2022-09-18 21:39:42 +0000

    Area and perimeter of rectangle program

---
+++ area_per_rect.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created By: Marcus Wehbi
+# Date: Sep. 18, 2022
+# This program finds the area and perimeter of a rectangle.
+
+def main():
+  # This function calculates area and perimeter
+  print("Area and perimeter of a rectangle - ")
+  print(" ")
+  print("If a rectangle has the dimensions:")
+  print("17cm x 9cm")
+  print(" ")
+  print("The area is: {}cm^2".format(17 * 9))
+  print(" ")
+  print("The perimeter is: {}cm".format(2*(17+9)))
+  
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created By: Marcus Wehbi
+# Date: Sep. 18, 2022
+# This program does basic math.
+
+def main():
+  # This function does basic math
+  print("Basic Math Operators:")
+  print("Addition: 7.34 + 21.9 = {}".format(7.34 + 21.9))
+  print("Subtraction: 37.2 - 14.5 = {}".format(37.2 - 14.5))
+  print("Multiplication: 13.6 x 3 = {}".format(13.6 * 3))
+  print("Division: 81 / 9 = {}".format(81 / 9))
+  print("Powers: 7 ** 11 = {}".format(7 ** 11))
+  print("Roots: 121 √ 11 = {}".format(121 // 11))
+
+if __name__ == "__main__":
+    main()
